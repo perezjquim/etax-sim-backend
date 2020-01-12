70c7d3f75741df082170141c37a67af6d4ee6f21
LuisFreitas97 <lgf1997@outlook.pt>
2020-01-12 10:00:08 +0000

    Adapter core.

---
+++ etax_sim/Adapter/SimulAdapter.cs
@@ -0,0 +1,16 @@
+﻿using eTaxSim.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Primitives;
+using System;
+using System.Collections.Generic;
+
+namespace eTaxSim.Adapter
+{
+    public class SimulAdapter : IAdapter
+    {
+        public object OnAdapt()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
