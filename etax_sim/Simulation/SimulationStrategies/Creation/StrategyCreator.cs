ec3f6dae5c54ba8dbe10e03f0d9b5bc560fed799
João Borges <joao.pedro.borges@hotmail.com>
2020-01-16 00:28:46 +0000

    log4net package configurations

--- etax_sim/Simulation/SimulationStrategies/Creation/StrategyCreator.cs
+++ etax_sim/Simulation/SimulationStrategies/Creation/StrategyCreator.cs
@@ -1,12 +1,15 @@
 ﻿using eTaxSim.Models;
 using System;
 using System.Collections.Generic;
+using System.Text;
 
 namespace eTaxSim.Simulation.SimulationStrategies.Creation
 {
     public class StrategyCreator
     {
         private readonly AppDbContext _context;
+
+        private const string BASE_NAMESPACE = "eTaxSim.Simulation.SimulationStrategies";
 
         public StrategyCreator(AppDbContext aContext)
         {
@@ -15,27 +18,71 @@ namespace eTaxSim.Simulation.SimulationStrategies.Creation
 
         public IStrategy FactoryMethod(Country aCountry, Region aRegion, Strategy aStrategy, IDictionary<string, object> aParametersDictionary)
         {
-            var strategy = FindImplementClass(aStrategy.ImplementingClass);
+
+
+            //var strategy = FindImplementClass(aStrategy.ImplementingClass);
             strategy.SetStrategyParameters(aCountry, aRegion, aParametersDictionary);
 
             return strategy;
         }
+        // eTaxSim.Simulation.SimulationStrategies
+        // eTaxSim.Simulation.SimulationStrategies.LiquidSalary.Portugal.Madeira.LiquidSalaryMadeira
+        // eTaxSim.Simulation.SimulationStrategies.TaxReturn.Portugal.TaxReturnPortugal
 
-        protected IStrategy FindImplementClass(string aClassName)
+        protected IStrategy FindImplementClass(string aClassName, Country aCountry, Region aRegion)
         {
             IStrategy myObject;
+
+            var className = CreateClassName(aClassName, aRegion.Name);
+            var strategyPath = CreateStrategyPath(aCountry.Name, aRegion.Name);
+
+            var completePath = CreateCompletePath(BASE_NAMESPACE, strategyPath, className);
             try
             {
-                var type = Type.GetType(aClassName);
+                var type = Type.GetType(completePath);
                 myObject = (IStrategy)Activator.CreateInstance(type);
             }
-            catch (Exception e)
+            catch (Exception)
             {
-                var argEx = new ArgumentException("Class name not found", aClassName, e);
-                throw argEx;
+                try
+                {
+                    className = CreateClassName(aClassName, aCountry.Name);
+                    strategyPath = CreateStrategyPath(aCountry.Name, null);
+
+                    completePath = CreateCompletePath(BASE_NAMESPACE, strategyPath, className);
+
+                    var type = Type.GetType(completePath);
+                    myObject = (IStrategy)Activator.CreateInstance(type);
+                }
+                catch (Exception e)
+                {
+                    var argEx = new ArgumentException("Class name not found", aClassName, e);
+                    throw argEx;
+                }
             }
 
             return myObject;
+        }
+
+        private string CreateCompletePath(string basePath, string strategyPath, string className)
+        {
+            var completePath = basePath + "." + strategyPath + "." + className;
+            return completePath;
+        }
+
+        private string CreateStrategyPath(string aCountry, string aRegion)
+        {
+            StringBuilder strategyPath = new StringBuilder(aCountry);
+            if (!string.IsNullOrEmpty(aRegion))
+            {
+                strategyPath.Append(".").Append(aRegion);
+            }
+            return strategyPath.ToString();
+        }
+
+        private string CreateClassName(string aClassName, string aLocation)
+        {
+            return aClassName + aLocation;
         }
     }
 }
