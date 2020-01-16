ee0a4cdbf34c16d615c23b13aaf2e2adaeaabf3c
LuisFreitas97 <lgf1997@outlook.pt>
2020-01-16 21:17:32 +0000

    Migration fix.

---
+++ etax_sim/Migrations/20200116203115_new35.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace eTaxSim.Migrations
+{
+    public partial class new35 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                 name: "StrategyByCountryId",
+                 table: "Country");
+
+            migrationBuilder.DropColumn(
+                 name: "StrategyByCountryByRegionId",
+                 table: "Country");
+
+            migrationBuilder.DropColumn(
+                 name: "StrategyByCountryByRegionId",
+                 table: "Region");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                 name: "StrategyByCountryId",
+                 table: "Country");
+
+            migrationBuilder.DropColumn(
+                 name: "StrategyByCountryByRegionId",
+                 table: "Country");
+
+            migrationBuilder.DropColumn(
+                 name: "StrategyByCountryByRegionId",
+                 table: "Region");
+        }
+    }
+}
